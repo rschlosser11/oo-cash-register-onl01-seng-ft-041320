5c305e791c53ea0705ea432baa1efc28b6f546b5
global-event-2762 <rosalinschlosser@gmail.com>
2020-04-22 16:56:47 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,7 +9,7 @@ class CashRegister
 
   def add_item(title, price, quantity = 1)
     quantity.times {@@all_items << title}
-    @last_transaction = price
+    @last_transaction = price * quantity
     @total += price * quantity
   end
 
